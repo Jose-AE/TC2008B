d4195b504348cb9546a0820a05c00287fb0ea355
JoseAE <107892696+Jose-AE@users.noreply.github.com>
2024-11-03 23:53:48 -0600

    Improve vector ops file

--- Assets/Scirpts/VectorOperations.cs
+++ Assets/Scirpts/VectorOperations.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
+
+
+
 public class VectorOperations
 {
+
+    private enum VectorOperationTypes
+    {
+
+    }
+
     public static float DotProduct(Vector3 a, Vector3 b)
     {
         return a.x * b.x + a.y * a.z * b.z;
 
-    }
-
-    public static Vector3 Centroid(Vector3 a, Vector3 b, Vector3 c)
-    {
-        return a + b + c / 3;
     }
 
     public static Vector3 CrossProduct(Vector3 a, Vector3 b)
@@ -26,7 +30,7 @@ public class VectorOperations
         );
     }
 
-    public static Matrix4x4 TranslationMatrix(Vector3 transform)
+    public static Matrix4x4 GetTranslationMatrix(Vector3 transform)
     {
         Matrix4x4 matrix = Matrix4x4.identity;
         matrix[0, 3] = transform.x;
@@ -36,7 +40,7 @@ public class VectorOperations
     }
 
 
-    public static Matrix4x4 ScaleMatrix(Vector3 scale)
+    public static Matrix4x4 GetScaleMatrix(Vector3 scale)
     {
         Matrix4x4 matrix = Matrix4x4.identity;
         matrix[0, 0] = scale.x;
@@ -46,7 +50,7 @@ public class VectorOperations
     }
 
 
-    public static Matrix4x4 RotateXMatrix(float degrees)
+    public static Matrix4x4 GetXRotationMatrix(float degrees)
     {
         Matrix4x4 matrix = Matrix4x4.identity;
         matrix[1, 1] = Mathf.Cos(Mathf.Deg2Rad * degrees);
@@ -59,7 +63,7 @@ public class VectorOperations
         return matrix;
     }
 
-    public static Matrix4x4 RotateYMatrix(float degrees)
+    public static Matrix4x4 GetYRotationMatrix(float degrees)
     {
         Matrix4x4 matrix = Matrix4x4.identity;
         matrix[0, 0] = Mathf.Cos(Mathf.Deg2Rad * degrees);
@@ -71,7 +75,7 @@ public class VectorOperations
         return matrix;
     }
 
-    public static Matrix4x4 RotateZMatrix(float degrees)
+    public static Matrix4x4 GetZRotationMatrix(float degrees)
     {
         Matrix4x4 matrix = Matrix4x4.identity;
         matrix[0, 0] = Mathf.Cos(Mathf.Deg2Rad * degrees);
@@ -82,6 +86,70 @@ public class VectorOperations
 
         return matrix;
     }
+
+
+
+    public static Vector3 Scale(Vector3 vector, Vector3 scale)
+    {
+        Vector4 homogeneousVector = vector;
+        homogeneousVector.w = 1;
+
+        Matrix4x4 matrix = GetScaleMatrix(scale);
+
+        return matrix * homogeneousVector;
+    }
+
+    public static Vector3 Translate(Vector3 vector, Vector3 units)
+    {
+        Vector4 homogeneousVector = vector;
+        homogeneousVector.w = 1;
+
+        Matrix4x4 matrix = GetTranslationMatrix(units);
+
+        return matrix * homogeneousVector;
+    }
+
+    public static Vector3 RotateX(Vector3 vector, float degrees, Vector3 pivot)
+    {
+        Vector3 translatedVector = vector - pivot;
+
+        Vector4 homogeneousVector = new Vector4(translatedVector.x, translatedVector.y, translatedVector.z, 1);
+
+        Matrix4x4 matrix = GetXRotationMatrix(degrees);
+
+        Vector4 rotatedVector = matrix * homogeneousVector;
+
+        return new Vector3(rotatedVector.x, rotatedVector.y, rotatedVector.z) + pivot;
+    }
+
+    public static Vector3 RotateY(Vector3 vector, float degrees, Vector3 pivot)
+    {
+        Vector3 translatedVector = vector - pivot;
+
+        Vector4 homogeneousVector = new Vector4(translatedVector.x, translatedVector.y, translatedVector.z, 1);
+
+        Matrix4x4 matrix = GetYRotationMatrix(degrees);
+
+        Vector4 rotatedVector = matrix * homogeneousVector;
+
+        return new Vector3(rotatedVector.x, rotatedVector.y, rotatedVector.z) + pivot;
+    }
+
+
+    public static Vector3 RotateZ(Vector3 vector, float degrees, Vector3 pivot)
+    {
+        Vector3 translatedVector = vector - pivot;
+
+        Vector4 homogeneousVector = new Vector4(translatedVector.x, translatedVector.y, translatedVector.z, 1);
+
+        Matrix4x4 matrix = GetZRotationMatrix(degrees);
+
+        Vector4 rotatedVector = matrix * homogeneousVector;
+
+        return new Vector3(rotatedVector.x, rotatedVector.y, rotatedVector.z) + pivot;
+    }
+
+
 
 
 }
